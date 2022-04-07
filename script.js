bd157b96aa690ba885035bf6b8a7b17eaa7306c7
Caitlin Swickard <caitlin6029@gmail.com>
2022-04-07 16:46:47 -0600

    created random select function to highlight tags

--- script.js
+++ script.js
@@ -2,20 +2,32 @@
 const tagsEl = document.getElementById('tags');
 const textarea = document.getElementById('textarea');
 
+
 // set automatic focus for textarea
 textarea.focus();
+
 
 // event listener for textarea
 textarea.addEventListener('keyup', (e) => {
   // captures what we type into text area
   createTags(e.target.value)
+
+  // if the key pressed is Enter, call randomSelect function, then clear the text area after 10ms
+  if (e.key === 'Enter') {
+    setTimeout(() => {
+      e.target.value = ''
+    }, 10)
+
+    randomSelect();
+  }
 })
+
 
 // function to capture textarea input 
 function createTags(input) {
   // filter = can not be an empty string and alos trim nay white space
-  const tags = input.split(',').filter(tag => tag.trim()!== '').map(tag => tag.trim())
-  
+  const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+
   // clear text area
   tagsEl.innerHTML = '';
 
@@ -25,6 +37,51 @@ function createTags(input) {
     tagEl.classList.add('tag');
     tagEl.innerText = tag;
     tagsEl.appendChild(tagEl);
-
   })
 }
+
+
+// function for the random selection of the choice
+function randomSelect() {
+  // highlight 30 times before landing on a pick
+  const times = 30;
+
+  // every 100ms picks and highlights through a random tag
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag()
+
+    highlightTag(randomTag);
+    // then unhighlights the tag
+    setTimeout(() => {
+      unHighlightTag(randomTag)
+    }, 100)
+  }, 100);
+
+  // stops our randomSelect function
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag()
+      highlightTag(randomTag)
+    }, 100)
+  }, times * 100)
+};
+
+
+// takes all the tags and uses math random to select though the array length
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag')
+  return tags[Math.floor(Math.random() * tags.length)]
+};
+
+
+// adds the highligh class list as random tag picker works through array
+function highlightTag(tag) {
+  tag.classList.add('highlight')
+};
+
+// removes the highlight class list
+function unHighlightTag(tag) {
+  tag.classList.remove('highlight')
+};
